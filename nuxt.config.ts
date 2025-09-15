fd2f062ce0b53e2389d74cb6b5378b74e2a392ed
ilya Pinchuk <deffokorn71@gmail.com>
2025-09-15 18:12:38 +0300

    added baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
+    devtools: { enabled: true },
     components: true,
     css: [
         '@/assets/styles/main.scss'
     ],
+    app: {
+        baseURL: 'https://ilyapinchuk.github.io/nuxt_test/'
+    }
 })
